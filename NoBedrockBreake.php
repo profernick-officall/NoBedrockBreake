d12c150cfee558dd835e7c02a6f33a2f136b8c51
profernick-officall <114315465+profernick-officall@users.noreply.github.com>
2023-03-20 19:10:09 +0700

    NoBedrockBreake

---
+++ NoBedrockBreake.php
@@ -0,0 +1,30 @@
+<?php
+
+/**
+ * @author profernick
+ * @name   NoBedrockBreake
+ * @main profernick\NoBedrockBreake
+ * @api 4.0.0
+ * @version 1.0.0
+ */
+
+namespace profernick;
+
+use pocketmine\event\EventPriority;
+use pocketmine\plugin\PluginBase;
+use pocketmine\event\block\BlockBreakEvent;
+use pocketmine\player\GameMode;
+use pocketmine\block\VanillaBlocks;
+
+class NoBedrockBreake extends PluginBase{
+
+    public function onEnable(): void{
+        $this->getServer()->getPluginManager()->registerEvent(BlockBreakEvent::class, function(BlockBreakEvent $event) : void{
+            if($event->getBlock()->isSameType(VanillaBlocks::BEDROCK()) &&
+               $event->getPlayer()->getGamemode() == GameMode::CREATIVE()){
+                $event->cancel();
+            }
+        }, EventPriority::NORMAL, $this, true);
+    }
+
+}
